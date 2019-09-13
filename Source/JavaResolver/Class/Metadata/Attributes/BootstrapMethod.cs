96faf8bfe6a61aa27303702b1857ca357da6e0f2
Washi <washi1337@protonmail.com>
2019-09-13 14:20:46 +0200

    Bootstrap methods attribute.

---
+++ Source/JavaResolver/Class/Metadata/Attributes/BootstrapMethod.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+
+namespace JavaResolver.Class.Metadata.Attributes
+{
+    /// <summary>
+    /// Represents a single entry in the bootstrap methods attribute.
+    /// </summary>
+    public class BootstrapMethod
+    {
+        /// <summary>
+        /// Reads a single bootstrap methods attribute at the current position of the provided reader.
+        /// </summary>
+        /// <param name="reader">The reader to use.</param>
+        /// <returns>The bootstrap methods attribute that was read.</returns>
+        public static BootstrapMethod FromReader(IBigEndianReader reader)
+        {
+            var result = new BootstrapMethod
+            {
+                MethodRefIndex = reader.ReadUInt16(),
+            };
+
+            ushort count = reader.ReadUInt16();
+            for (int i = 0; i < count; i++)
+                result.Arguments.Add(reader.ReadUInt16());
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets or sets the raw index into the constant pool referencing the method to be called.
+        /// </summary>
+        public ushort MethodRefIndex
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets a mutable collection of indices to static arguments to be passed onto the bootstrap method.
+        /// </summary>
+        public IList<ushort> Arguments
+        {
+            get;
+        } = new List<ushort>();
+
+        /// <summary>
+        /// Writes the entry to an output stream.
+        /// </summary>
+        /// <param name="writer">The writer to use.</param>
+        public void Write(IBigEndianWriter writer)
+        {
+            writer.Write(MethodRefIndex);
+            writer.Write((ushort) Arguments.Count);
+            foreach (ushort argument in Arguments)
+                writer.Write(argument);
+        }
+        
+    }
+}
---
+++ Source/JavaResolver/Class/Metadata/Attributes/BootstrapMethodsAttribute.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.IO;
+using JavaResolver.Class.Emit;
+
+namespace JavaResolver.Class.Metadata.Attributes
+{
+    /// <summary>
+    /// Represents the bootstrap methods attribute containing a table of boot strap methods used by the invokedynamic opcode.
+    /// </summary>
+    public class BootstrapMethodsAttribute : IAttributeContents
+    {
+        public const string AttributeName = "BootstrapMethods";
+
+        /// <summary>
+        /// Reads a single bootstrap methods attribute at the current position of the provided reader.
+        /// </summary>
+        /// <param name="reader">The reader to use.</param>
+        /// <returns>The bootstrap methods attribute that was read.</returns>
+        public static BootstrapMethodsAttribute FromReader(IBigEndianReader reader)
+        {
+            var result = new BootstrapMethodsAttribute();
+
+            ushort count = reader.ReadUInt16();
+            for (int i = 0; i < count; i++)
+                result.BootstrapMethods.Add(BootstrapMethod.FromReader(reader));
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public string Name => AttributeName;
+
+        /// <summary>
+        /// Gets the ordered collection of bootstrap methods defined by this attribute.
+        /// </summary>
+        public IList<BootstrapMethod> BootstrapMethods
+        {
+            get;
+        } = new List<BootstrapMethod>();
+
+        /// <inheritdoc />
+        public byte[] Serialize(BuildingContext context)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var writer = new BigEndianStreamWriter(stream);
+                writer.Write((ushort) BootstrapMethods.Count);
+                foreach (var method in BootstrapMethods)
+                    method.Write(writer);
+                return stream.ToArray();
+            }
+        }
+        
+    }
+}
